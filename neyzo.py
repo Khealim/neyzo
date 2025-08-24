217d075324d15ade15fdfcc658ae27c980e30849
Khealim <83818858+Khealim@users.noreply.github.com>
2025-08-24 15:22:41 +0200

    Update neyzo.py

--- neyzo.py
+++ neyzo.py
@@ -5,8 +5,13 @@ import random
 st.title("Neyzo kiszopik?")
 
 if st.button("Roll the dice 🎲"):
-    result = random.choice(["igen, fostosra szopja a gémet ", "nem szopik, daliás győzelmet arat"])
-    st.subheader(result)
+    result = random.choice(["Taknyosra verik", "Daliás győzelmet arat"])
+
+    if "győzelmet" in result:
+        st.success(result)   # green box
+    else:
+        st.error(result)     # red box
+
 
 st.subheader("Bárhogy is legyen nézzed a sztrinyot paraszt")
 st_player("https://www.twitch.tv/neyzo_d2")
@@ -14,5 +19,6 @@ st_player("https://www.twitch.tv/neyzo_d2")
 st.title("Nincs kedved nézni?")
 with st.expander("Nézzed meg ezt azt lesz"):
     st.video("https://www.youtube.com/watch?v=LdeMWatu4k8")
+
 
 
