09a873a79efa9de60df4e5d61826c39cc2eab1cf
Khealim <83818858+Khealim@users.noreply.github.com>
2025-08-24 15:20:49 +0200

    Update neyzo.py

--- neyzo.py
+++ neyzo.py
@@ -2,20 +2,17 @@ import streamlit as st
 from streamlit_player import st_player
 import random
 
-# Title
 st.title("Neyzo kiszopik?")
 
-# 50/50 roll
 if st.button("Roll the dice 🎲"):
     result = random.choice(["igen, fostosra szopja a gémet ", "nem szopik, daliás győzelmet arat"])
     st.subheader(result)
 
-
-# Embed Twitch stream
-st.subheader("Bérhogy is legyen nézzed a sztrinyot paraszt")
+st.subheader("Bárhogy is legyen nézzed a sztrinyot paraszt")
 st_player("https://www.twitch.tv/neyzo_d2")
 
 st.title("Nincs kedved nézni?")
 with st.expander("Nézzed meg ezt azt lesz"):
     st.video("https://www.youtube.com/watch?v=LdeMWatu4k8")
+
 
